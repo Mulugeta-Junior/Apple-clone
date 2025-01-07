b74450d6d16ecbd8478deb3c52a391ab293b6e48
Mulugeta-Junior <mulugeta.kassa2112@gmail.com>
2025-01-07 22:31:22 +0300

    Footer six and third section updated

--- src/commonResource/main body/Sixthsection.js
+++ src/commonResource/main body/Sixthsection.js
@@ -1,52 +1,120 @@
 
-import arcade from "../images/icons/arcade.png";
+// import arcade from "../images/icons/arcade.png";
 
+// import React from "react";
+
+// function Sixthsection() {
+//   return (
+//     <div>
+//       <section class="sixth-heghlight-wrapper">
+//         <div class="container-fluid">
+//           <div class="row">
+//             <div class="left-side-wrapper col-sm-12 col-md-6">
+//               <div class="left-side-container">
+//                 <div class="top-logo-wrapper">
+//                   <div class="logo-wrapper">
+//                     <img src={arcade} />
+//                   </div>
+//                 </div>
+//                 <div class="description-wraper white">
+//                   Agent 8 is a small hero on a big mission.
+//                 </div>
+//                 <div class="links-wrapper">
+//                   <ul>
+//                     <li>
+//                       <a href="#">
+//                         Play now<sup>2</sup>
+//                       </a>
+//                     </li>
+//                     <li>
+//                       <a href="#">Learn about Apple Arcade</a>
+//                     </li>
+//                   </ul>
+//                 </div>
+//               </div>
+//             </div>
+//             <div class="right-side-wrapper col-sm-12 col-md-6">
+//               <div class="right-side-container">
+//                 <div class="title-wraper">Apple Card Monthly Installments</div>
+//                 <div class="description-wraper">
+//                   Pay for your next iPhone over time, interest-free with Apple
+//                   Card.
+//                 </div>
+//                 <div class="links-wrapper">
+//                   <ul>
+//                     <li>
+//                       <a href="#">Learn more</a>
+//                     </li>
+//                     <li>
+//                       <a href="#">Apply now</a>
+//                     </li>
+//                   </ul>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </section>
+//     </div>
+//   );
+// }
+
+// export default Sixthsection;
+
+
+import arcade from "../images/icons/arcade.png";
 import React from "react";
 
 function Sixthsection() {
   return (
     <div>
-      <section class="sixth-heghlight-wrapper">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="left-side-wrapper col-sm-12 col-md-6">
-              <div class="left-side-container">
-                <div class="top-logo-wrapper">
-                  <div class="logo-wrapper">
-                    <img src={arcade} />
+      <section className="sixth-heghlight-wrapper">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="left-side-wrapper col-sm-12 col-md-6">
+              <div className="left-side-container">
+                <div className="top-logo-wrapper">
+                  <div className="logo-wrapper">
+                    <img src={arcade} alt="Arcade Logo" />
                   </div>
                 </div>
-                <div class="description-wraper white">
+                <div className="description-wraper white">
                   Agent 8 is a small hero on a big mission.
                 </div>
-                <div class="links-wrapper">
+                <div className="links-wrapper">
                   <ul>
                     <li>
-                      <a href="">
+                      <a href="#" onClick={(e) => e.preventDefault()}>
                         Play now<sup>2</sup>
                       </a>
                     </li>
                     <li>
-                      <a href="">Learn about Apple Arcade</a>
+                      <a href="#" onClick={(e) => e.preventDefault()}>
+                        Learn about Apple Arcade
+                      </a>
                     </li>
                   </ul>
                 </div>
               </div>
             </div>
-            <div class="right-side-wrapper col-sm-12 col-md-6">
-              <div class="right-side-container">
-                <div class="title-wraper">Apple Card Monthly Installments</div>
-                <div class="description-wraper">
+            <div className="right-side-wrapper col-sm-12 col-md-6">
+              <div className="right-side-container">
+                <div className="title-wraper">Apple Card Monthly Installments</div>
+                <div className="description-wraper">
                   Pay for your next iPhone over time, interest-free with Apple
                   Card.
                 </div>
-                <div class="links-wrapper">
+                <div className="links-wrapper">
                   <ul>
                     <li>
-                      <a href="">Learn more</a>
+                      <a href="#" onClick={(e) => e.preventDefault()}>
+                        Learn more
+                      </a>
                     </li>
                     <li>
-                      <a href="">Apply now</a>
+                      <a href="#" onClick={(e) => e.preventDefault()}>
+                        Apply now
+                      </a>
                     </li>
                   </ul>
                 </div>
@@ -60,3 +128,4 @@ function Sixthsection() {
 }
 
 export default Sixthsection;
+
--- src/commonResource/main body/Thirdsection.js
+++ src/commonResource/main body/Thirdsection.js
@@ -1,27 +1,70 @@
+// import React from "react";
+
+// function Thirdsection() {
+//   return (
+//     <div>
+//       <section class="third-hightlight-wrapper">
+//         <div class="container">
+//           <div class="title-wraper bold">iPhone 11 Pro</div>
+
+//           <div class="description-wrapper">
+//             Pro cameras. Pro display. Pro performance.
+//           </div>
+
+//           <div class="price-wrapper">
+//             From $24.95/mo. or $599 with trade‑in.
+//           </div>
+
+//           <div class="links-wrapper">
+//             <ul>
+//               <li>
+//                 <a href="#">Learn more</a>
+//               </li>
+//               <li>
+//                 <a href="#">Buy</a>
+//               </li>
+//             </ul>
+//           </div>
+//         </div>
+//       </section>
+//     </div>
+//   );
+// }
+
+// export default Thirdsection;
+
+
+
+
+
 import React from "react";
 
 function Thirdsection() {
   return (
     <div>
-      <section class="third-hightlight-wrapper">
-        <div class="container">
-          <div class="title-wraper bold">iPhone 11 Pro</div>
+      <section className="third-hightlight-wrapper">
+        <div className="container">
+          <div className="title-wraper bold">iPhone 11 Pro</div>
 
-          <div class="description-wrapper">
+          <div className="description-wrapper">
             Pro cameras. Pro display. Pro performance.
           </div>
 
-          <div class="price-wrapper">
+          <div className="price-wrapper">
             From $24.95/mo. or $599 with trade‑in.
           </div>
 
-          <div class="links-wrapper">
+          <div className="links-wrapper">
             <ul>
               <li>
-                <a href="">Learn more</a>
+                <a href="#" onClick={(e) => e.preventDefault()}>
+                  Learn more
+                </a>
               </li>
               <li>
-                <a href="">Buy</a>
+                <a href="#" onClick={(e) => e.preventDefault()}>
+                  Buy
+                </a>
               </li>
             </ul>
           </div>
@@ -32,3 +75,4 @@ function Thirdsection() {
 }
 
 export default Thirdsection;
+
